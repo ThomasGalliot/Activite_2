52a0ba710a9e23d1531a4b590d1e6c6603313285
ThomasGalliot <thomas.galliot@hotmail.fr>
2018-07-11 14:09:18 +0200

    azerty

--- activite_2/js/contacts.js
+++ activite_2/js/contacts.js
@@ -11,9 +11,13 @@ alert("Bienvenue dans le gestionaire des contacts !");
 while (option !== "0"){
     option = prompt("choisisez une option:\n1: Liste des contact\n2: Ajouté un contact\n0: quitter");
     
-    if (option === "1") {
+    if (option < 0 || option > 2)
+        alert("Cette option n'éxiste pas");
+
+    else if (option === "1") {
         alert("Voici la liste de tout vos contact:\n" + formulaire); 
     }
+
     else if(option === "2") {
         var prenom = prompt("entrer le prénom du nouveau contact:");
         formulaire.push("\nprénom: " + prenom);
