a301945a4e6a210c44c1aa50efd38346d470a743
ThomasGalliot <thomas.galliot@hotmail.fr>
2018-07-11 14:25:43 +0200

    azerty

--- activite_2/js/contacts.js
+++ activite_2/js/contacts.js
@@ -1,8 +1,3 @@
-/* 
-Activité : gestion des contacts
-*/
-
-// TODO : complétez le programme
 var formulaire = ["prénom: Carole", " nom: Lévisse", "\nprénom: Mélodie", " nom: Nelsonne"];
 var option = "";
 
@@ -10,13 +5,12 @@ alert("Bienvenue dans le gestionaire des contacts !");
 
 while (option !== "0"){
     option = prompt("choisisez une option:\n1: Liste des contact\n2: Ajouté un contact\n0: quitter");
-    
+
     if (option < 0 || option > 2)
         alert("Cette option n'éxiste pas!");
 
-    else if (option === "1") {
-        alert("Voici la liste de tout vos contact:\n" + formulaire); 
-    }
+    else if (option === "1")
+        alert("Voici la liste de tout vos contact:\n" + formulaire);
 
     else if(option === "2") {
         var prenom = prompt("entrer le prénom du nouveau contact:");
@@ -25,6 +19,6 @@ while (option !== "0"){
         var nom = prompt("entrer le nom de votre nouveau contact:");
         formulaire.push(" nom: " + nom);
 
-        alert("le contact a bien été ajouté"); 
+        alert("le contact a bien été ajouté");
     }
 }
